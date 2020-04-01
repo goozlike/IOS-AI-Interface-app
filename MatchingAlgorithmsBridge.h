d9ea95b300f722cbc68d62ed74d4de8fcd6e0003
goozlike <58444268+goozlike@users.noreply.github.com>
2020-04-01 14:17:00 +0300

    Update and rename LaneDetectorBridge.h to MatchingAlgorithmsBridge.h

---
+++ MatchingAlgorithmsBridge.h
@@ -0,0 +1,24 @@
+//
+//  MatchingAlgorithmsBridge.h
+//  project_app
+//
+//  Created by Никита Борисов on 30.03.2020.
+//  Copyright © 2020 Arina Goloubitskaya. All rights reserved.
+//
+
+#ifndef MatchingAlgorithmsBridge_h
+#define MatchingAlgorithmsBridge_h
+
+
+#endif /* MatchingAlgorithmsBridge_h */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MatchingAlgorithmsBridge : NSObject
+
+- (UIImage *) findBest: (UIImage *) image;
+
+- (UIImage *) Match: (UIImage *) image;
+
+@end
